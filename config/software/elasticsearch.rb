e6f006be35d0864d7c266771ef8a303eacc496f8
Marius Sturm <marius@torch.sh>
2014-10-20 17:30:15 +0200

    bump to 0.91.1

--- config/software/elasticsearch.rb
+++ config/software/elasticsearch.rb
@@ -1,13 +1,13 @@
 name "elasticsearch"
-default_version "0.90.10"
+default_version "1.3.4"
 
 dependency "server-jre"
 dependency "runit"
 
-source url: "https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-0.90.10.tar.gz",
-       md5: "a74bbc6f751ef891c0d3f408dad346cb"
+source url: "https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.3.4.tar.gz",
+       md5: "4d718cc9db428486ab06e19fc098b196"
 
-relative_path "elasticsearch-0.90.10"
+relative_path "elasticsearch-1.3.4"
 
 build do
   mkdir  "#{install_dir}/elasticsearch"
--- config/software/graylog2-web.rb
+++ config/software/graylog2-web.rb
@@ -1,13 +1,13 @@
 name "graylog2-web"
-default_version "0.90.0"
+default_version "0.91.1"
 
 dependency "server-jre"
 dependency "runit"
 
-source url: "http://packages.graylog2.org/releases/graylog2-web-interface/graylog2-web-interface-0.90.0.tgz",
-       md5: "2ec07487ce6a98d7ec63c52888bb1ab4"
+source url: "http://packages.graylog2.org/releases/graylog2-web-interface/graylog2-web-interface-0.91.1.tgz",
+       md5: "a7d1387fd34ae2f031448f469147bd31"
 
-relative_path "graylog2-web-interface-0.90.0"
+relative_path "graylog2-web-interface-0.91.1"
 
 build do
   mkdir "#{install_dir}/web"
