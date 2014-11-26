0ba6a7d0cd51b728dd339d38b2e7fc74cd08b2de
Marius Sturm <marius.sturm@googlemail.com>
2014-11-26 19:07:19 +0100

    install etcd ruby client

--- config/software/etcd.rb
+++ config/software/etcd.rb
@@ -1,7 +1,10 @@
 name "etcd"
 default_version "v0.5.0-alpha.3"
+gem_version = "0.2.4"
 
 dependency "openssl"
+dependency "ruby"
+dependency "rubygems"
 
 source :url => "https://github.com/coreos/etcd/releases/download/#{version}/etcd-#{version}-linux-amd64.tar.gz",
        :md5 => "1f6f6ecd9f1800da73fb897901c83cf6"
@@ -11,4 +14,6 @@ relative_path "etcd-#{version}-linux-amd64"
 build do
   copy "#{project_dir}/etcdctl", "#{install_dir}/embedded/bin"
   copy "#{project_dir}/etcd", "#{install_dir}/embedded/sbin"
+
+  gem "install etcd -n #{install_dir}/embedded/bin --no-rdoc --no-ri -v #{gem_version}"
 end
