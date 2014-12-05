5adb2fe9a76a2453412f8656627ed91878d30a9e
Marius Sturm <marius@torch.sh>
2014-12-05 18:11:54 +0100

    create data directory with correct ownership

--- files/graylog2-cookbooks/graylog2/recipes/users.rb
+++ files/graylog2-cookbooks/graylog2/recipes/users.rb
@@ -2,12 +2,6 @@ graylog2_username = node['graylog2']['user']['username']
 graylog2_group    = node['graylog2']['user']['group']
 graylog2_home     = node['graylog2']['user']['home']
 
-directory graylog2_home do
-  owner graylog2_username
-  group graylog2_group
-  recursive true
-end
-
 # Create the group for the Graylog2 user
 group graylog2_group do
   gid node['graylog2']['user']['gid']
@@ -16,6 +10,7 @@ end
 # Create the Graylog2 user
 user graylog2_username do
   shell node['graylog2']['user']['shell']
+  supports :manage_home => true
   home graylog2_home
   uid node['graylog2']['user']['uid']
   gid graylog2_group
@@ -28,4 +23,10 @@ template File.join(graylog2_home, ".gitconfig") do
   group graylog2_group
   mode "0644"
   variables(node['graylog2']['user'].to_hash)
+end
+
+# create additional directories
+directory "#{graylog2_home}/data" do
+  owner graylog2_username
+  group graylog2_group
 end
