d58f1bbeacc1662aa79791c1ecf513d19d31e6e9
Marius Sturm <marius@torch.sh>
2014-12-05 18:10:45 +0100

    update to latest etcd

--- config/software/etcd.rb
+++ config/software/etcd.rb
@@ -1,5 +1,5 @@
 name "etcd"
-default_version "v0.5.0-alpha.3"
+default_version "v0.5.0-alpha.4"
 gem_version = "0.2.4"
 
 dependency "openssl"
@@ -7,7 +7,7 @@ dependency "ruby"
 dependency "rubygems"
 
 source :url => "https://github.com/coreos/etcd/releases/download/#{version}/etcd-#{version}-linux-amd64.tar.gz",
-       :md5 => "1f6f6ecd9f1800da73fb897901c83cf6"
+       :md5 => "16993f1016daf55ed802b92f7daedbaa"
 
 relative_path "etcd-#{version}-linux-amd64"
 
