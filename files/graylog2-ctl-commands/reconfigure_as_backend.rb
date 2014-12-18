3be05d88cc9bf5ad49c57ad5428b11caf80629b0
Marius Sturm <marius.sturm@googlemail.com>
2014-12-18 21:21:31 +0100

    add server/ES combined role

---
+++ files/graylog2-ctl-commands/reconfigure_as_backend.rb
@@ -0,0 +1,34 @@
+add_command 'reconfigure-as-backend', 'Run Graylog2 server and Elasticsearch on this node', 1 do |cmd_name|
+  require 'fileutils'
+  require 'json'
+
+  if true
+    existing_services ||= Hash.new
+    if File.exists?("/etc/graylog2/graylog2-services.json")
+      existing_services = JSON.parse(File.read("/etc/graylog2/graylog2-services.json"))
+    else
+      FileUtils.mkdir_p("/etc/graylog2")
+      existing_services['etcd']            = Hash.new
+      existing_services['nginx']           = Hash.new
+      existing_services['mongodb']         = Hash.new
+      existing_services['elasticsearch']   = Hash.new
+      existing_services['graylog2_server'] = Hash.new
+      existing_services['graylog2_web']    = Hash.new
+    end
+
+    existing_services['etcd']['enabled']            = true
+    existing_services['nginx']['enabled']           = false
+    existing_services['mongodb']['enabled']         = true
+    existing_services['elasticsearch']['enabled']   = true
+    existing_services['graylog2_server']['enabled'] = true
+    existing_services['graylog2_web']['enabled']    = false
+
+    File.open("/etc/graylog2/graylog2-services.json","w") do |services|
+      services.write(JSON.pretty_generate(existing_services))
+    end
+
+    reconfigure
+  else
+    puts "Usage: #{cmd_name}"
+  end
+end
