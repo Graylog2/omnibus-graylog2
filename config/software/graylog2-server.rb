704a55c1f7711d044e547e6f18018da67600bc2d
Marius Sturm <marius.sturm@googlemail.com>
2015-01-15 15:31:48 +0100

    add additional project for beta packages

--- config/software/graylog2-server.rb
+++ config/software/graylog2-server.rb
@@ -4,8 +4,13 @@ default_version project.build_version
 dependency "server-jre"
 dependency "runit"
 
-source url: "http://packages.graylog2.org/releases/graylog2-server/graylog2-server-#{version}.tgz",
-       md5: "777d53f5f4f12e9e6ce8374d99fe0176"
+if version.include? '-beta' or version.include? '-rc'
+  source url: "http://packages.graylog2.org/releases/graylog2-server/graylog2-server-#{version}.tgz",
+         md5: "afd98d8653e04e1ea13db2590fdb915e"
+else
+  source url: "http://packages.graylog2.org/releases/graylog2-server/graylog2-server-#{version}.tgz",
+         md5: "777d53f5f4f12e9e6ce8374d99fe0176"
+end
 
 relative_path "graylog2-server-#{version}"
 
--- config/software/graylog2-web.rb
+++ config/software/graylog2-web.rb
@@ -4,8 +4,13 @@ default_version project.build_version
 dependency "server-jre"
 dependency "runit"
 
-source url: "http://packages.graylog2.org/releases/graylog2-web-interface/graylog2-web-interface-#{version}.tgz",
-       md5: "811138fe8b7b2bd061e865a4775d9951"
+if version.include? '-beta' or version.include? '-rc'
+  source url: "http://packages.graylog2.org/releases/graylog2-web-interface/graylog2-web-interface-#{version}.tgz",
+         md5: "5def5a2870673af7114d6be919410fc3"
+else
+  source url: "http://packages.graylog2.org/releases/graylog2-web-interface/graylog2-web-interface-#{version}.tgz",
+         md5: "811138fe8b7b2bd061e865a4775d9951"
+end
 
 relative_path "graylog2-web-interface-#{version}"
 
