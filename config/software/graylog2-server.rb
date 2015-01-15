36bdf6badd0fc3db5328a0196a9f8525007e3ff2
Marius Sturm <marius.sturm@googlemail.com>
2015-01-15 15:51:51 +0100

    fix extraction path

--- config/software/graylog2-server.rb
+++ config/software/graylog2-server.rb
@@ -7,12 +7,15 @@ dependency "runit"
 if version.include? '-beta' or version.include? '-rc'
   source url: "http://packages.graylog2.org/releases/graylog2-server/graylog2-server-#{version}.tgz",
          md5: "afd98d8653e04e1ea13db2590fdb915e"
+  
+  relative_path "graylog2-#{version}"
 else
   source url: "http://packages.graylog2.org/releases/graylog2-server/graylog2-server-#{version}.tgz",
          md5: "777d53f5f4f12e9e6ce8374d99fe0176"
+
+  relative_path "graylog2-server-#{version}"
 end
 
-relative_path "graylog2-server-#{version}"
 
 build do
   mkdir "#{install_dir}/server"
