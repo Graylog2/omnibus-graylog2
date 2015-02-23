ce3af69317901da75595939d538d2798b6c06ec5
Marius Sturm <marius.sturm@googlemail.com>
2015-02-23 13:12:00 +0100

    optimize ES memory settings

--- config/projects/graylog.rb
+++ config/projects/graylog.rb
@@ -4,7 +4,7 @@ homepage 'https://graylog.org'
 
 install_dir     '/opt/graylog'
 build_version   '1.0.0'
-build_iteration  1
+build_iteration  2
 
 override :ruby,       version: "2.1.4"
 override :'chef-gem', version: "12.0.3"
