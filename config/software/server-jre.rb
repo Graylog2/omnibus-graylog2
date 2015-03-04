65551024356a893fa6a6153f9053988adb518bf9
Marius Sturm <marius@graylog.com>
2015-03-04 17:27:22 +0100

    update to Java 8

--- config/software/server-jre.rb
+++ config/software/server-jre.rb
@@ -15,7 +15,7 @@
 #
 
 name "server-jre"
-default_version "7u67"
+default_version "8u40"
 
 whitelist_file "jre/bin/javaws"
 whitelist_file "jre/bin/policytool"
@@ -25,18 +25,17 @@ whitelist_file "jre/bin/appletviewer"
 
 if _64_bit?
   # TODO: download x86 version on x86 machines
-  source url:     "http://download.oracle.com/otn-pub/java/jdk/7u71-b14/server-jre-7u71-linux-x64.tar.gz",
-         md5:     "2e29846da3297c9b2469d69017363bd7",
+  source url:     "http://download.oracle.com/otn-pub/java/jdk/8u31-b13/server-jre-8u40-linux-x64.tar.gz",
+         md5:     "fa788615f0cb83df6c81761775fb2a7a",
          cookie:  "gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie",
          warning: "By including the JRE, you accept the terms of the Oracle Binary Code License Agreement for the Java SE Platform Products and JavaFX, which can be found at http://www.oracle.com/technetwork/java/javase/terms/license/index.html"
 else
   raise "Server-jre can only be installed on x86_64 systems."
 end
 
-relative_path "jdk1.7.0_71"
+relative_path "jdk1.8.0_40"
 
 build do
   mkdir "#{install_dir}/embedded/jre"
   sync  "#{project_dir}/", "#{install_dir}/embedded/jre"
 end
-
