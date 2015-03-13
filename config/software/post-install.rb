e4d94a17e149e7ee05d7ceec725a3d0deda2782e
Marius Sturm <marius@graylog.com>
2015-03-13 14:08:44 +0100

    fix file names

--- config/software/post-install.rb
+++ config/software/post-install.rb
@@ -6,7 +6,7 @@ source :path => File.expand_path("files/docker", Config.project_root)
 
 build do
   mkdir "#{install_dir}/embedded/share/docker"
-  copy "#{project_dir}/docker-environment", "#{install_dir}/embedded/share/docker/"
-  copy "#{project_dir}/01_run_graylogctl.sh", "#{install_dir}/embedded/share/docker/"
+  copy "#{project_dir}/docker_environment", "#{install_dir}/embedded/share/docker/"
+  copy "#{project_dir}/run_graylogctl", "#{install_dir}/embedded/share/docker/"
   copy "#{project_dir}/my_init", "#{install_dir}/embedded/share/docker/"
 end
