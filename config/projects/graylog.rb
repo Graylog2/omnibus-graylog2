f0acb7bcb7100724634909ba7d18ab80dc20d08c
Marius Sturm <marius@graylog.com>
2015-04-17 14:42:24 +0200

    bump build iteration

--- config/projects/graylog.rb
+++ config/projects/graylog.rb
@@ -4,7 +4,7 @@ homepage 'https://graylog.org'
 
 install_dir     '/opt/graylog'
 build_version   '1.0.1'
-build_iteration  6
+build_iteration  7
 
 override :ruby,       version: "2.1.4"
 override :'chef-gem', version: "12.0.3"
