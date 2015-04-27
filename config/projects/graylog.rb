12c011b1aac9ef288d5b849cbb64d2dfc1ecb6b7
Marius Sturm <marius@graylog.com>
2015-04-27 14:53:11 +0200

    fix path

--- config/projects/graylog.rb
+++ config/projects/graylog.rb
@@ -4,7 +4,7 @@ homepage 'https://graylog.org'
 
 install_dir     '/opt/graylog'
 build_version   '1.0.1'
-build_iteration  8
+build_iteration  9
 
 override :ruby,       version: "2.1.4"
 override :'chef-gem', version: "12.0.3"
--- config/software/elasticsearch.rb
+++ config/software/elasticsearch.rb
@@ -7,7 +7,7 @@ dependency "runit"
 source url: "https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.4.5.tar.gz",
        md5: "01b6f3317c50a1b6964a56494363cc49"
 
-relative_path "elasticsearch-1.4.4"
+relative_path "elasticsearch-1.4.5"
 
 build do
   mkdir  "#{install_dir}/elasticsearch"
