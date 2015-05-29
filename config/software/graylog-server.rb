f6d71be99bc0fc29fc32fc5993e47d2c24fc5a0b
Marius Sturm <marius@graylog.com>
2015-05-29 17:30:22 +0200

    bump to 1.1.0-rc.1

--- config/software/graylog-server.rb
+++ config/software/graylog-server.rb
@@ -6,7 +6,7 @@ dependency "runit"
 
 if version.include? '-beta' or version.include? '-rc'
   source url: "http://packages.graylog2.org/releases/graylog2-server/graylog-#{version}.tgz",
-         md5: "3343d39961de636dce7188ca017bacd8"
+         md5: "2c41640fb7c4b390115508a9bf9edc26"
 else
   source url: "http://packages.graylog2.org/releases/graylog2-server/graylog-#{version}.tgz",
          md5: "fe23c4b598c72a557dd5f6d8e6ed8bb2"
--- config/software/graylog-web.rb
+++ config/software/graylog-web.rb
@@ -6,7 +6,7 @@ dependency "runit"
 
 if version.include? '-beta' or version.include? '-rc'
   source url: "http://packages.graylog2.org/releases/graylog2-web-interface/graylog-web-interface-#{version}.tgz",
-         md5: "d00148604278cd3ba2be2c3ac440c98b"
+         md5: "3842479a20e4d73be85407e13edb2c9c"
 else
   source url: "http://packages.graylog2.org/releases/graylog2-web-interface/graylog-web-interface-#{version}.tgz",
          md5: "0f9efc572cd79079f909abccf397e345"
