8c82be5afca7d4d154705424fb2443ab491a3aef
Marius Sturm <marius.sturm@googlemail.com>
2015-06-04 14:42:57 +0200

    bump to 1.1.0

--- config/software/graylog-web.rb
+++ config/software/graylog-web.rb
@@ -9,7 +9,7 @@ if version.include? '-beta' or version.include? '-rc'
          md5: "b208795f93b93d56fd9bbbe9a4852d96"
 else
   source url: "http://packages.graylog2.org/releases/graylog2-web-interface/graylog-web-interface-#{version}.tgz",
-         md5: "0f9efc572cd79079f909abccf397e345"
+         md5: "28c18a423a538ddf7d5ff4db868615d8"
 end
 
 relative_path "graylog-web-interface-#{version}"
