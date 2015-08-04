91d275271536a598a6f8409db21df4f86477189b
Marius Sturm <marius@graylog.com>
2015-08-04 11:48:26 +0200

    bump to ES 1.7.1

--- config/software/elasticsearch.rb
+++ config/software/elasticsearch.rb
@@ -1,13 +1,13 @@
 name "elasticsearch"
-default_version "1.7.0"
+default_version "1.7.1"
 
 dependency "server-jre"
 dependency "runit"
 
 source url: "https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.7.0.tar.gz",
-       md5: "c41c2d24907358d28b16ed4448c4eba4"
+       md5: "e086488d92a1d16a2a39236c72cf08fb"
 
-relative_path "elasticsearch-1.7.0"
+relative_path "elasticsearch-1.7.1"
 
 build do
   mkdir  "#{install_dir}/elasticsearch"
