d3abcda4b77239049594b5774fed90cd825a4f92
Marius Sturm <marius@graylog.com>
2015-08-04 11:52:50 +0200

    fix download url

--- config/software/elasticsearch.rb
+++ config/software/elasticsearch.rb
@@ -4,7 +4,7 @@ default_version "1.7.1"
 dependency "server-jre"
 dependency "runit"
 
-source url: "https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.7.0.tar.gz",
+source url: "https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.7.1.tar.gz",
        md5: "e086488d92a1d16a2a39236c72cf08fb"
 
 relative_path "elasticsearch-1.7.1"
