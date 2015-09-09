6713483fe5713a6ab927e78bea430b2baa57e32a
Marius Sturm <marius@graylog.com>
2015-09-09 17:16:31 +0200

    use version variable

--- config/software/mongodb.rb
+++ config/software/mongodb.rb
@@ -3,10 +3,10 @@ default_version "3.0.6"
 
 dependency "runit"
 
-source url: "https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-2.6.4.tgz",
+source url: "https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-#{version}.tgz",
        md5: "68f58028bb98ff7b97c4b37ebc20380c"
 
-relative_path "mongodb-linux-x86_64-3.0.6"
+relative_path "mongodb-linux-x86_64-#{version}"
 
 build do
   mkdir "#{install_dir}/mongodb"
