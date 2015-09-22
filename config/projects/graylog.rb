4ab49044d4156755e401b42933964ff4d5dc098b
Jochen Schalanda <jochen@graylog.com>
2015-09-22 14:57:24 +0200

    Update version to 1.2.1-1

--- config/projects/graylog.rb
+++ config/projects/graylog.rb
@@ -3,8 +3,8 @@ maintainer 'Marius Sturm'
 homepage 'https://graylog.org'
 
 install_dir     '/opt/graylog'
-build_version   '1.2.0'
-build_iteration  2
+build_version   '1.2.1'
+build_iteration  1
 
 override :ruby,       version: "2.1.4"
 override :'chef-gem', version: "12.4.1"
