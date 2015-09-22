fc31c080467a5cbed10791c206f8f14effc4d813
Jochen Schalanda <jochen@graylog.com>
2015-09-22 13:57:40 +0200

    Update to Elasticsearch 1.7.2

--- config/software/elasticsearch.rb
+++ config/software/elasticsearch.rb
@@ -1,13 +1,13 @@
 name "elasticsearch"
-default_version "1.7.1"
+default_version "1.7.2"
 
 dependency "server-jre"
 dependency "runit"
 
-source url: "https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.7.1.tar.gz",
-       md5: "e086488d92a1d16a2a39236c72cf08fb"
+source url: "https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-1.7.2.tar.gz",
+       md5: "cfadd0ab444b754446f95a9ec86b688a"
 
-relative_path "elasticsearch-1.7.1"
+relative_path "elasticsearch-1.7.2"
 
 build do
   mkdir  "#{install_dir}/elasticsearch"
