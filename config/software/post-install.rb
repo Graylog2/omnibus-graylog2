12ed6e4f4a5548156f531cd9c03ae270d12df7d7
Marius Sturm <marius@graylog.com>
2015-11-04 18:19:28 +0100

    ship nginx content pack

--- config/software/post-install.rb
+++ config/software/post-install.rb
@@ -2,11 +2,12 @@ name "post-install"
 
 always_build true
 
-source :path => File.expand_path("files/docker", Config.project_root)
+source :path => File.expand_path("files", Config.project_root)
 
 build do
   mkdir "#{install_dir}/embedded/share/docker"
-  copy "#{project_dir}/docker_environment", "#{install_dir}/embedded/share/docker/"
-  copy "#{project_dir}/run_graylogctl", "#{install_dir}/embedded/share/docker/"
-  copy "#{project_dir}/my_init", "#{install_dir}/embedded/share/docker/"
+  copy "#{project_dir}/docker/docker_environment", "#{install_dir}/embedded/share/docker/"
+  copy "#{project_dir}/docker/run_graylogctl", "#{install_dir}/embedded/share/docker/"
+  copy "#{project_dir}/docker/my_init", "#{install_dir}/embedded/share/docker/"
+  sync "#{project_dir}/data/contentpacks/", "#{install_dir}/contentpacks/"
 end
