cab112ae65408d87884eba296734ffb52f5991db
Marius Sturm <marius@graylog.com>
2015-11-04 13:33:35 +0100

    pin web interface version

--- config/software/graylog-web.rb
+++ config/software/graylog-web.rb
@@ -5,8 +5,9 @@ dependency "server-jre"
 dependency "runit"
 
 if version.include? '-beta' or version.include? '-rc'
-  source url: "http://packages.graylog2.org/releases/graylog2-web-interface/graylog-web-interface-#{version}.tgz",
-         md5: "6e393ab4ced367af3f51015b7a7be5ef"
+  #source url: "http://packages.graylog2.org/releases/graylog2-web-interface/graylog-web-interface-#{version}.tgz",
+  source url: "https://packages.graylog2.org/nightly-builds/graylog-web-interface-1.3.0-SNAPSHOT-20151103144707.tgz",
+         md5: "cd2ae650e3c37589671f21123fd121ac"
 else
   source url: "http://packages.graylog2.org/releases/graylog2-web-interface/graylog-web-interface-#{version}.tgz",
          md5: "ffde989ec88cf41097a7083454d2d894"
