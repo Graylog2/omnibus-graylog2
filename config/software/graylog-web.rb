f013a2f4503ccf92829a78fc71bb82b958a992f3
Marius Sturm <marius@graylog.com>
2015-11-04 13:38:42 +0100

    build web interface snapshot version

--- config/software/graylog-web.rb
+++ config/software/graylog-web.rb
@@ -4,7 +4,7 @@ default_version project.build_version
 dependency "server-jre"
 dependency "runit"
 
-if version.include? '-beta' or version.include? '-rc'
+if version.include? '-beta' or version.include? '-rc' or version.include? 'SNAPSHOT'
   #source url: "http://packages.graylog2.org/releases/graylog2-web-interface/graylog-web-interface-#{version}.tgz",
   source url: "https://packages.graylog2.org/nightly-builds/graylog-web-interface-1.3.0-SNAPSHOT-20151103144707.tgz",
          md5: "cd2ae650e3c37589671f21123fd121ac"
