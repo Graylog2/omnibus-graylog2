826ac97f4d8d25da8d4df1cc39fabd461aa2627e
Marius Sturm <marius@graylog.com>
2015-11-24 15:59:24 +0100

    update snapshot version

--- config/projects/graylog-beta.rb
+++ config/projects/graylog-beta.rb
@@ -3,7 +3,7 @@ maintainer 'Marius Sturm'
 homepage 'https://graylog.org'
 
 install_dir     '/opt/graylog'
-build_version   '1.3.0-SNAPSHOT-20151118121016'
+build_version   '1.3.0-SNAPSHOT-20151120133125'
 build_iteration  1
 
 override :ruby,       version: "2.1.4"
--- config/software/graylog-web.rb
+++ config/software/graylog-web.rb
@@ -6,7 +6,7 @@ dependency "runit"
 
 if version.include? '-beta' or version.include? '-rc' or version.include? 'SNAPSHOT'
   #source url: "http://packages.graylog2.org/releases/graylog2-web-interface/graylog-web-interface-#{version}.tgz",
-  source url: "https://packages.graylog2.org/nightly-builds/graylog-web-interface-1.3.0-SNAPSHOT-20151118142643.tgz",
+  source url: "https://packages.graylog2.org/nightly-builds/graylog-web-interface-1.3.0-SNAPSHOT-20151123175017.tgz",
          md5: "2aee69361b5fcda60628d3b2b8e7c72b"
 else
   source url: "http://packages.graylog2.org/releases/graylog2-web-interface/graylog-web-interface-#{version}.tgz",
@@ -14,7 +14,7 @@ else
 end
 
 #relative_path "graylog-web-interface-#{version}"
-relative_path "graylog-web-interface-1.3.0-SNAPSHOT-20151118142643"
+relative_path "graylog-web-interface-1.3.0-SNAPSHOT-20151123175017"
 
 build do
   mkdir "#{install_dir}/web"
