d7adde7ad8d0b3476898c16f004ae7c9c7791bc3
Marius Sturm <marius@graylog.com>
2016-01-20 16:42:33 +0100

    pin kopf version

--- config/software/kopf.rb
+++ config/software/kopf.rb
@@ -1,12 +1,12 @@
 name "kopf"
-default_version "1.0"
+default_version "1.6.2"
 
 dependency "elasticsearch"
 
-source url: "https://github.com/lmenezes/elasticsearch-kopf/archive/1.0.zip",
-       md5: "73210c723911eaf7e92726dd869b8152"
+source url: "https://github.com/lmenezes/elasticsearch-kopf/archive/v#{version}.zip",
+       md5: "2d912373f350f980893e42266daed53f"
 
-relative_path "elasticsearch-kopf-1.0"
+relative_path "elasticsearch-kopf-#{version}"
 
 build do
   mkdir  "#{install_dir}/elasticsearch/plugins/kopf"
