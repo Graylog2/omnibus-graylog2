6392f49d07d3f684658fe4d6e64363d279dbd475
Marius Sturm <marius@graylog.com>
2016-03-01 16:11:55 +0100

    update openssl lib

--- config/projects/graylog.rb
+++ config/projects/graylog.rb
@@ -9,10 +9,10 @@ build_iteration  1
 override :ruby,       version: "2.1.8",
                         source: { md5: "091b62f0a9796a3c55de2a228a0e6ef3" }
 override :'chef-gem', version: "12.6.0"
-override :'openssl',  version: "1.0.1r",
+override :'openssl',  version: "1.0.1s",
                       source: {
-                        url: "https://www.openssl.org/source/openssl-1.0.1r.tar.gz",
-                        md5: "1abd905e079542ccae948af37e393d28" }
+                        url: "https://www.openssl.org/source/openssl-1.0.1s.tar.gz",
+                        md5: "562986f6937aabc7c11a6d376d8a0d26" }
 override :'cacerts',  version: "2015.10.28",
                       source: { md5: "6f41fb0f0c4b4695c2a6296892278141" }
 
