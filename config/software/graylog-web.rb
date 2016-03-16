1129fe545069be98b5c088755944ea1d6826ea0a
Jochen Schalanda <jochen@graylog.com>
2016-03-16 15:59:37 +0100

    Update to Graylog 1.3.4

--- config/software/graylog-web.rb
+++ config/software/graylog-web.rb
@@ -9,7 +9,7 @@ if version.include? '-beta' or version.include? '-rc' or version.include? 'SNAPS
          md5: "47a4445586fef210699b7dbb9c8558c9"
 else
   source url: "http://packages.graylog2.org/releases/graylog2-web-interface/graylog-web-interface-#{version}.tgz",
-         md5: "0ea7b5108aba6d973fa583c51a06aacd"
+         md5: "bbfe7edb069582bf926d7cbc5abd42d9"
 end
 
 relative_path "graylog-web-interface-#{version}"
