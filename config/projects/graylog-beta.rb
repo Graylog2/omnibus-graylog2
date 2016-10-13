54839d52afb7f3503484f8b39af57cfd1bb0cf2b
Marius Sturm <marius@graylog.com>
2016-10-13 15:44:02 +0200

    bump to 2.2.0-alpha.1

--- config/projects/graylog-beta.rb
+++ config/projects/graylog-beta.rb
@@ -3,20 +3,20 @@ maintainer 'Marius Sturm'
 homepage 'https://graylog.org'
 
 install_dir     '/opt/graylog'
-build_version   '2.1.0-rc.1'
+build_version   '2.2.0-alpha.1'
 build_iteration  1
 
 override :ruby,       version: "2.1.8",
                         source: { md5: "091b62f0a9796a3c55de2a228a0e6ef3" }
 override :'chef-gem', version: "12.6.0"
-override :'openssl',  version: "1.0.1t",
+override :'openssl',  version: "1.0.1u",
                       source: {
-                        url: "https://www.openssl.org/source/openssl-1.0.1t.tar.gz",
-                        md5: "9837746fcf8a6727d46d22ca35953da1" }
-override :'cacerts',  version: "2016-04-20",
+                        url: "https://www.openssl.org/source/openssl-1.0.1u.tar.gz",
+                        md5: "130bb19745db2a5a09f22ccbbf7e69d0" }
+override :'cacerts',  version: "2016-09-14",
                       source: {
-                        url: "https://curl.haxx.se/ca/cacert-2016-04-20.pem",
-                        md5: "782dcde8f5d53b1b9e888fdf113c42b9" }
+                        url: "https://curl.haxx.se/ca/cacert-2016-09-14.pem",
+                        md5: "8d35a5cef6ce28da07867a0712558067" }
 
 dependency "prepare"
 dependency "chef-gem"
--- config/software/graylog-server.rb
+++ config/software/graylog-server.rb
@@ -6,7 +6,7 @@ dependency "runit"
 
 if version.include? '-alpha' or version.include? '-beta' or version.include? '-rc' or version.include? 'SNAPSHOT'
   source url: "http://packages.graylog2.org/releases/graylog/graylog-#{version}.tgz",
-         md5: "da1b7705a14f87e3d23f5efee9d8d967"
+         md5: "46d67405e4be37219a536dcc55f6a758"
 else
   source url: "http://packages.graylog2.org/releases/graylog/graylog-#{version}.tgz",
          md5: "d877cfb9672dd1163671e0f0bd2caea1"
