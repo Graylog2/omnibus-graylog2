fe71f8ffeb50bbe275e20c9fbda0cc97923b45c9
Marius Sturm <marius@graylog.com>
2017-01-13 14:50:55 +0100

    Bump MongoDB version and add client gem

--- config/software/mongodb.rb
+++ config/software/mongodb.rb
@@ -1,14 +1,19 @@
 name "mongodb"
-default_version "3.2.5"
+default_version "3.4.1"
+gem_version = "2.4.1"
 
 dependency "runit"
+dependency "ruby"
+dependency "rubygems"
 
 source url: "https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-#{version}.tgz",
-       md5: "aecc4de30d37903776ba192f4359c717"
+       md5: "8f5ce8185e14ff54bde0984305e1833c"
 
 relative_path "mongodb-linux-x86_64-#{version}"
 
 build do
   mkdir "#{install_dir}/mongodb"
   sync  "#{project_dir}/", "#{install_dir}/mongodb"
+
+  gem "install mongo -n #{install_dir}/embedded/bin --no-rdoc --no-ri -v #{gem_version}"
 end
