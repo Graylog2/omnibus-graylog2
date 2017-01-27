397a3c11ee99ea59a07b8ab4eaf73d8dc1195437
Marius Sturm <marius@graylog.com>
2017-01-27 14:22:44 +0100

    fix recipe

--- config/software/rsync.rb
+++ config/software/rsync.rb
@@ -3,13 +3,8 @@ default_version "3.1.2"
 
 dependency "popt"
 
-license_file "COPYING"
-skip_transitive_dependency_licensing true
-
 version "3.1.2" do
   source md5: "0f758d7e000c0f7f7d3792610fad70cb"
-  license "GPL-3.0"
-  license_file "COPYING"
 end
 
 source url: "https://rsync.samba.org/ftp/rsync/src/rsync-#{version}.tar.gz"
