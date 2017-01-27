ae5a0ad0d792bd299afbf3e0a322fb7c636e8fd7
Marius Sturm <marius@graylog.com>
2017-01-27 14:55:00 +0100

    pin net-ssh-gateway version

--- config/software/chef-gem.rb
+++ config/software/chef-gem.rb
@@ -7,6 +7,7 @@ dependency "libffi"
 dependency "ohai"
 
 build do
+  gem "install net-ssh-gateway -n #{install_dir}/embedded/bin --no-rdoc --no-ri -v 1.3.0"
   gem "install chef-config -n #{install_dir}/embedded/bin --no-rdoc --no-ri -v #{version}"
   gem "install chef -n #{install_dir}/embedded/bin --no-rdoc --no-ri -v #{version}"
 end
