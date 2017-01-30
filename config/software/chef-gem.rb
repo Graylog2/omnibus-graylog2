f6b748a5629af4c417f432e72234c6418c9dc981
Marius Sturm <marius@graylog.com>
2017-01-30 14:04:49 +0100

    pin more gems

--- config/software/chef-gem.rb
+++ config/software/chef-gem.rb
@@ -7,7 +7,10 @@ dependency "libffi"
 dependency "ohai"
 
 build do
-  gem "install net-ssh-gateway -n #{install_dir}/embedded/bin --no-rdoc --no-ri -v 1.3.0"
+  gem "install net-ssh -n #{install_dir}/embedded/bin --no-rdoc --no-ri -v 2.9.4"
+  gem "install net-ssh-gateway -n #{install_dir}/embedded/bin --no-rdoc --no-ri -v 1.2.0"
+  gem "install net-ssh-multi -n #{install_dir}/embedded/bin --no-rdoc --no-ri -v 1.2.1"
+  gem "install rack -n #{install_dir}/embedded/bin --no-rdoc --no-ri -v 1.6.4"
   gem "install chef-config -n #{install_dir}/embedded/bin --no-rdoc --no-ri -v #{version}"
   gem "install chef -n #{install_dir}/embedded/bin --no-rdoc --no-ri -v #{version}"
 end
