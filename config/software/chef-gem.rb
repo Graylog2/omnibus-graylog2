c9098149f6a86755dd347cc6c3850e6d0faecce4
Marius Sturm <marius@graylog.com>
2017-01-31 15:01:16 +0100

    Set chef gems versions

--- config/software/chef-gem.rb
+++ config/software/chef-gem.rb
@@ -7,6 +7,10 @@ dependency "libffi"
 dependency "ohai"
 
 build do
+  gem "install net-ssh -n #{install_dir}/embedded/bin --no-rdoc --no-ri -v 2.10.0"
+  gem "install net-ssh-gateway -n #{install_dir}/embedded/bin --no-rdoc --no-ri -v 1.3.0"
+  gem "install net-ssh-multi -n #{install_dir}/embedded/bin --no-rdoc --no-ri -v 1.2.1"
+  gem "install rack -n #{install_dir}/embedded/bin --no-rdoc --no-ri -v 1.6.5"
   gem "install chef-config -n #{install_dir}/embedded/bin --no-rdoc --no-ri -v #{version}"
   gem "install chef -n #{install_dir}/embedded/bin --no-rdoc --no-ri -v #{version}"
 end
