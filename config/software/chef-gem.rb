ee53c4b7c4a92b701b6bb9455288939e8a250f93
Marius Sturm <marius@graylog.com>
2017-01-31 15:15:25 +0100

    Use available version

--- config/software/chef-gem.rb
+++ config/software/chef-gem.rb
@@ -7,7 +7,7 @@ dependency "libffi"
 dependency "ohai"
 
 build do
-  gem "install net-ssh -n #{install_dir}/embedded/bin --no-rdoc --no-ri -v 2.10.0"
+  gem "install net-ssh -n #{install_dir}/embedded/bin --no-rdoc --no-ri -v 2.9.4"
   gem "install net-ssh-gateway -n #{install_dir}/embedded/bin --no-rdoc --no-ri -v 1.3.0"
   gem "install net-ssh-multi -n #{install_dir}/embedded/bin --no-rdoc --no-ri -v 1.2.1"
   gem "install rack -n #{install_dir}/embedded/bin --no-rdoc --no-ri -v 1.6.5"
