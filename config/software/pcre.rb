565483a0994cf51354b3969d6f02481d5f505880
Jochen Schalanda <jochen@graylog.com>
2017-07-18 10:30:06 +0200

    Upgrade to PCRE 8.41

--- config/software/pcre.rb
+++ config/software/pcre.rb
@@ -1,13 +1,13 @@
 name "pcre"
-default_version "8.39"
+default_version "8.41"
 
 dependency "libedit"
 dependency "ncurses"
 
-source :url => "ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre-8.39.tar.gz",
-       :md5 => "26a76d97e04c89fe9ce22ecc1cd0b315"
+source :url => "ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre-8.41.tar.gz",
+       :md5 => "2e7896647ee25799cb454fe287ffcd08"
 
-relative_path "pcre-8.39"
+relative_path "pcre-8.41"
 
 configure_env = {
   "CFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include"
