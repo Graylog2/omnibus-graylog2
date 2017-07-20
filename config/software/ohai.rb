c2be28800583f8efa13e78799559a92deacfca01
Marius Sturm <marius@graylog.com>
2017-07-20 11:51:12 +0200

    Fix ohai build

--- config/software/ohai.rb
+++ config/software/ohai.rb
@@ -7,5 +7,6 @@ dependency "libffi"
 
 build do
   gem "install ffi-yajl -n #{install_dir}/embedded/bin --no-rdoc --no-ri -v 2.3.0"
+  gem "install mixlib-shellout -n #{install_dir}/embedded/bin --no-rdoc --no-ri -v 2.2.7"
   gem "install ohai -n #{install_dir}/embedded/bin --no-rdoc --no-ri -v #{version}"
 end
