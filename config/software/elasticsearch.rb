75d1e8a1746fa48cb50a19ed7c0437b33276ecb3
Jochen Schalanda <jochen@schalanda.name>
2017-08-04 11:44:56 +0200

    Revert "Elasticsearch migration plugin (#42)"
    This reverts commit d09184cc7ed792dbb560a74e6536c5b13021b169.

--- config/software/elasticsearch.rb
+++ config/software/elasticsearch.rb
@@ -1,11 +1,11 @@
 name "elasticsearch"
-default_version "2.4.6"
+default_version "2.4.2"
 
 dependency "jdk"
 dependency "runit"
 
 source url: "https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-#{version}.tar.gz",
-       md5: "f527b5a50c188ad98acc37f6b4e97b90"
+       md5: "e2ae0152109c9d78d7aa83b63ffcf2ab"
 
 relative_path "elasticsearch-#{version}"
 
