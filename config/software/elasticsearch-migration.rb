c82bf5e46f5ec95bb14b3e0a93fd7e8402565736
Jochen Schalanda <jochen@graylog.com>
2017-08-04 10:32:43 +0200

    Add missing comma

--- config/software/elasticsearch-migration.rb
+++ config/software/elasticsearch-migration.rb
@@ -3,7 +3,7 @@ default_version "2.0.4"
 
 dependency "elasticsearch"
 
-source url: "https://github.com/elastic/elasticsearch-migration/releases/download/v#{version}/elasticsearch-migration-#{version}.zip"
+source url: "https://github.com/elastic/elasticsearch-migration/releases/download/v#{version}/elasticsearch-migration-#{version}.zip",
        md5: "907c3b39b6511692c3e4c9a9f2c79507"
 
 build do
