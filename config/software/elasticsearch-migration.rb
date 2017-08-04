d09184cc7ed792dbb560a74e6536c5b13021b169
Jochen Schalanda <jochen@schalanda.name>
2017-08-04 10:53:55 +0200

    Elasticsearch migration plugin (#42)
    * Add Elasticsearch Migration plugin
    
    https://github.com/elastic/elasticsearch-migration/tree/2.x
    
    * Upgrade to Elasticsearch 2.4.6
    
    https://www.elastic.co/guide/en/elasticsearch/reference/2.4/release-notes-2.4.3.html
    https://www.elastic.co/guide/en/elasticsearch/reference/2.4/release-notes-2.4.4.html
    https://www.elastic.co/guide/en/elasticsearch/reference/2.4/release-notes-2.4.5.html
    https://www.elastic.co/guide/en/elasticsearch/reference/2.4/release-notes-2.4.6.html
    
    * Add missing comma

---
+++ config/software/elasticsearch-migration.rb
@@ -0,0 +1,12 @@
+name "elasticsearch-migration"
+default_version "2.0.4"
+
+dependency "elasticsearch"
+
+source url: "https://github.com/elastic/elasticsearch-migration/releases/download/v#{version}/elasticsearch-migration-#{version}.zip",
+       md5: "907c3b39b6511692c3e4c9a9f2c79507"
+
+build do
+  mkdir  "#{install_dir}/elasticsearch/plugins/migration"
+  sync   "#{project_dir}/", "#{install_dir}/elasticsearch/plugins/migration"
+end
