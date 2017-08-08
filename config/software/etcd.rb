a0397d26c2fcbb2904c0f104036e51407d37c8eb
Marius Sturm <marius@graylog.com>
2017-08-08 12:55:01 +0200

    Revert "Rolling etcd version back to 3.0 branch"
    This reverts commit 10f111b3a1453fa77a49e7706dad923dca2d8865.

--- config/software/etcd.rb
+++ config/software/etcd.rb
@@ -1,5 +1,5 @@
 name "etcd"
-default_version "v3.0.17"
+default_version "v3.2.4"
 gem_version = "0.3.0"
 
 dependency "openssl"
@@ -7,7 +7,7 @@ dependency "ruby"
 dependency "rubygems"
 
 source :url => "https://github.com/coreos/etcd/releases/download/#{version}/etcd-#{version}-linux-amd64.tar.gz",
-       :md5 => "0b9ae0d2fb96eabe612d6edd9bc0912a"
+       :md5 => "d1e92766a20439cc475d46e8024fdf7c"
 
 relative_path "etcd-#{version}-linux-amd64"
 
