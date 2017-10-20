b23707d4f9bc07d0733c5447af549dcde2157757
Marius Sturm <marius@graylog.com>
2017-10-20 17:20:52 +0200

    Fix server checksum

--- config/software/graylog-server.rb
+++ config/software/graylog-server.rb
@@ -6,7 +6,7 @@ dependency "runit"
 
 if version.include? '-alpha' or version.include? '-beta' or version.include? '-rc' or version.include? 'SNAPSHOT'
   source url: "http://packages.graylog2.org/releases/graylog/graylog-#{version}.tgz",
-         md5: "4fabdce1ed9ea25a6a0ba72ee8f057cb"
+         md5: "4ce195e31f23214f967f10e19cde581a"
 else
   source url: "http://packages.graylog2.org/releases/graylog/graylog-#{version}.tgz",
          md5: "c7d891a693ba3836734f5f62954b29cb"
