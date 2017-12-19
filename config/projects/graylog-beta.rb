033fe36430cb2a5b7113688675fbb05efadc4c13
Bernd Ahlers <bernd@graylog.com>
2017-12-19 16:02:37 +0100

    Bump to 2.4.0-rc.1

--- config/projects/graylog-beta.rb
+++ config/projects/graylog-beta.rb
@@ -3,7 +3,7 @@ maintainer 'Marius Sturm'
 homepage 'https://graylog.org'
 
 install_dir     '/opt/graylog'
-build_version   '2.4.0-beta.4'
+build_version   '2.4.0-rc.1'
 build_iteration  1
 
 conflict 'graylog-server'
