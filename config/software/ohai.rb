74d0b154dcdc0b786b03ed13ea64fa86190e55ec
Marius Sturm <marius@graylog.com>
2018-03-09 14:26:01 +0100

    Pin mixlib-config version to support ruby 2.1

--- config/software/ohai.rb
+++ config/software/ohai.rb
@@ -8,5 +8,6 @@ dependency "libffi"
 build do
   gem "install ffi-yajl -n #{install_dir}/embedded/bin --no-rdoc --no-ri -v 2.3.0"
   gem "install mixlib-shellout -n #{install_dir}/embedded/bin --no-rdoc --no-ri -v 2.2.7"
+  gem "install mixlib-config -n #{install_dir}/embedded/bin --no-rdoc --no-ri -v 2.2.4"
   gem "install ohai -n #{install_dir}/embedded/bin --no-rdoc --no-ri -v #{version}"
 end
