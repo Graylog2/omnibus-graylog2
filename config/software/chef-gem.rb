a8a03c64fa4ba63fcefa18fbb02e3e639dcfc2ec
Jochen Schalanda <jochen@graylog.com>
2018-07-17 10:07:00 +0200

    Ignore dependencies for net-ssh-multi Ruby gem
    This is a dirty hack around the following error:
    
    ```
    [Builder: chef-gem] I | Build chef-gem: 17.7083s
    /var/lib/gems/2.2.0/gems/mixlib-shellout-1.6.1/lib/mixlib/shellout.rb:272:in `invalid!': Expected process to exit with [0], but received '1' (Mixlib::ShellOut::ShellCommandFailed)
    ---- Begin output of /opt/graylog/embedded/bin/gem install net-ssh-multi -n /opt/graylog/embedded/bin --no-rdoc --no-ri -v 1.2.1 ----
    STDOUT:
    STDERR: ERROR:  Error installing net-ssh-multi:
    	net-ssh requires Ruby version >= 2.2.6.
    ```

--- config/software/chef-gem.rb
+++ config/software/chef-gem.rb
@@ -9,7 +9,7 @@ dependency "ohai"
 build do
   gem "install net-ssh -n #{install_dir}/embedded/bin --no-rdoc --no-ri -v 2.9.4"
   gem "install net-ssh-gateway -n #{install_dir}/embedded/bin --no-rdoc --no-ri -v 1.3.0"
-  gem "install net-ssh-multi -n #{install_dir}/embedded/bin --no-rdoc --no-ri -v 1.2.1"
+  gem "install net-ssh-multi --ignore-dependencies -n #{install_dir}/embedded/bin --no-rdoc --no-ri -v 1.2.1"
   gem "install rack -n #{install_dir}/embedded/bin --no-rdoc --no-ri -v 1.6.5"
   gem "install chef-config -n #{install_dir}/embedded/bin --no-rdoc --no-ri -v #{version}"
   gem "install chef -n #{install_dir}/embedded/bin --no-rdoc --no-ri -v #{version}"
